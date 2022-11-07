9028ce76668b4fca1139535e0910b69abb3c5f9a
Whiz75 <shanekhotsoka@gmail.com>
2022-11-07 15:35:18 +0200

    commit

--- KotaPalace/Fragments/ProfileFragment.cs
+++ KotaPalace/Fragments/ProfileFragment.cs
@@ -5,10 +5,14 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using AndroidHUD;
 using AndroidX.Fragment.App;
+using KotaPalace.Models;
+using KotaPalace_Api.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 
 namespace KotaPalace.Fragments
@@ -33,7 +37,51 @@ namespace KotaPalace.Fragments
 
             View rootView = inflater.Inflate(Resource.Layout.fragment_profile, container, false);
 
+            //Init(view);
+            GetUserDetails();
+
             return rootView;
+        }
+
+        private void Init(object view)
+        {
+            
+        }
+
+        private async void GetUserDetails()
+        {
+            //var Id = Preferences.Get("id", null);
+
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                var response = await httpClient.GetAsync($"{API.Url}/account/{Id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string str_out = await response.Content.ReadAsStringAsync();
+                    var user = Newtonsoft.Json.JsonConvert.DeserializeObject<AppUsers>(str_out);
+
+                    if (user != null)
+                    {
+                        //DriverNameTextView.Text = $"{user.Name} {user.Surname}";
+                        //InputNamesTextView.Text = user.Name;
+                        //InputSurnameTextView.Text = user.Surname;
+                        //InputPhoneTextView.Text = user.PhoneNumber;
+                        //InputEmailTextView.Text = user.Email;
+
+                    }
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Message(ex.Message);
+            }
+        }
+
+        private void Message(string message)
+        {
+            AndHUD.Shared.ShowError(Context, message, MaskType.None, TimeSpan.FromSeconds(3));
         }
     }
 }
